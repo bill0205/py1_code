2f88573d89d97271129f0ece3cf10b4d4b519845
bill <mrzhangdndnh@163.com>
2022-04-20 23:21:19 +0800

    advanced version

--- psd.py
+++ psd.py
@@ -8,16 +8,23 @@ i = 3
 #while 后面那个判断，true不true
 #要提前把值设好
 while i >= 1:
+	i = i - 1 #每问一次密码做一次回圈   
 	psd = input('请输入密码：')
 	if psd == password:  #字符串的相等
 		print('登陆成功')
 		break     #跳出回圈
 	else:
-		i = i - 1
-		print('密码错误!还有', i, '次机会')
-		if i == 1:
-			break
+		print('密码错误！')
+		if i > 0:   # 第三次不显示0
+			print('还有', i, '次机会')
+		else:
+			print('没机会尝试了，要锁账号了')
+
+#		if i == 0:
+#			break    #搭配while True：
 
 #怎么设计三次机会 用while
 #剩余0次怎么不打印 break
+# i = i - 1 放在else 会打印
+
 

fe1d6648e26a984e9278f8d0454c59d519457a81
bill <mrzhangdndnh@163.com>
2022-04-23 10:08:28 +0800

    input product and price

---
+++ 4product.py
@@ -0,0 +1,18 @@
+#使用者输入商品名称
+#用清单储存使用者输入的东西
+#清单怎么变成二维的，又装商品名又装价格
+
+products = []
+while True:
+	name = input('请输入商品名称:')
+	if name == 'q':
+		break
+	price = input('请输入产品的价格:')
+	#p = []
+	#p.append(name)
+	#p.append(price)
+	products.append([name,price])
+print(products)
+
+
+print(products[0][1])

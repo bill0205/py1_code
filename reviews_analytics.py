2bb1c8798e11daaed3f6c1312fd03474f8eb2443
bill <mrzhangdndnh@163.com>
2022-04-24 22:49:54 +0800

    add pip import

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -1,15 +1,24 @@
 
 
+import time #引入内建模组
+import progressbar
+#下载安装第三方的模组,要写class，创造资料类别type
+
+
 #不会把txt文件上传到git，360mb太大，git上限150
 data = []
 count = 0
+bar = progressbar.ProgressBar(maxval=10000000000) 
+bar.start()
 with open('reviews.txt','r') as f:
 	for line in f: 
 		#每次读取一行，装进清单
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+		bar.update(count)
+		#if count % 1000 == 0:
+		#	print(len(data))
+bar.finish()
 print('档案读取结束，一共有',len(data),'笔资料')
 
 #new = []
@@ -27,6 +36,7 @@ print('档案读取结束，一共有',len(data),'笔资料')
 
 #查找一个词出现过多少次 字典 key字 value次数
 #这部分很关键！！！ 如何做计数，字典的结构
+start_time = time.time()
 wc = {} #word_count
 for d in data: #d字串 data列表
 	words = d.split() #默认所有空格跳过
@@ -35,9 +45,12 @@ for d in data: #d字串 data列表
 			wc[word] += 1
 		else:
 			wc[word] = 1 #新增key进字典
+
 for word in wc:
 	if wc[word] > 1000000:
 		print(word,wc[word])
+end_time = time.time()
+print('一共花了',end_time - start_time,'秒')
 print(len(wc))
 print(wc['bill']) 
 
